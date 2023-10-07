96441edd87c236767b5364e4bd4a1ab290dd6db8
TSYSTANOV <tsystanov.vitaly@gmail.com>
2023-10-07 18:54:04 +0300

    first-time

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function()
+{
+    $('.head__slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:'<button type="button" class="slick-prev"><img src="images/prev.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="images/forw.png"></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+          
+            }
+          },
+
+          {
+            breakpoint: 640,
+            settings: {arrows:false
+            
+            }
+          }
+        ]
+      });
+})
