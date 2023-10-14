3f3e6de6f022d0d4845093e503d8d66ba7e643db
TSYSTANOV <tsystanov.vitaly@gmail.com>
2023-10-14 15:16:05 +0300

    добавил цитаты и адаптив

--- js/main.js
+++ js/main.js
@@ -23,3 +23,15 @@ $(function()
         ]
       });
 })
+//Comment
+$(function()
+{
+    $('.comment__wrap').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:'<button type="button" class="slick-prev"><img src="images/prev.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="images/forw.png"></button>',
+        arrows:false,
+      });
+})
